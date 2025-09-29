0d830f9c20a8be15c895fec29a3108b282240589
BMateusSs <bmdss0810@gmail.com>
2025-09-28 21:18:44 -0300

    Implementando a rota de login

--- backend/src/rotas/public.js
+++ backend/src/rotas/public.js
@@ -1,11 +1,19 @@
 import express from 'express'
+import login from '../database/querys/auth/login.js'
 
 const router = express.Router()
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     const user = req.body
 
-    res.send('otimo')
+    const result = await login(user.credential, user.password)
+
+    if (!result.status){
+        res.status(401).json({message: result.message})
+    }else{
+        res.status(200).json({userId: result.userId})
+    }
+
      
 })
 
