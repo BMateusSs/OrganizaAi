a56e6dcbca8b714a0fc09458dbaaaff85c8bc6b1
BMateusSs <bmdss0810@gmail.com>
2025-09-29 11:08:16 -0300

    Adicionando rota para cadastro de usuários

--- backend/src/rotas/public.js
+++ backend/src/rotas/public.js
@@ -1,7 +1,20 @@
 import express from 'express'
 import login from '../database/querys/auth/login.js'
+import register from '../database/querys/auth/register.js'
 
 const router = express.Router()
+
+router.post('/register', async (req, res) => {
+    const user = req.body
+
+    const result = await register(user.name, user.username, user.email, user.password)
+
+    if (!result.status){
+        res.status(401).json({message: result.message})
+    }else{
+        res.status(201).json({message: result.message})
+    }
+})
 
 router.post('/login', async (req, res) => {
     const user = req.body
@@ -14,7 +27,6 @@ router.post('/login', async (req, res) => {
         res.status(200).json({userId: result.userId})
     }
 
-     
 })
 
 export default router
