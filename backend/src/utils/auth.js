bd0bdcb370d7b07420ad5cc25b22050b168d60b8
BMateusSs <bmdss0810@gmail.com>
2025-09-29 14:55:32 -0300

    Funções para criar e validar token jwt

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -1,4 +1,6 @@
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
 
 export async function hashPassword(password){
     const salt = await bcryptjs.genSalt(10);
@@ -12,3 +14,29 @@ export async function verifyPassword(hash, password) {
     return result
 }
 
+export function createToken(userId){
+    const SECRET = process.env.SECRET_KEY
+    const payload = {userId: userId}
+    const options = {expiresIn: '2h'}
+    const token = jwt.sign(payload, SECRET, options)
+
+    return token
+}
+
+export function validateToken(req, res, next) {
+    const header = req.headers['authorization']
+    const token = header && header.split(" ")[1]
+
+    if (!token){
+        return res.status(401).json({message: 'Token não encontrado'})
+    }
+
+    try{
+        const SECRET = process.env.SECRET_KEY
+        const payload = jwt.verify(token, SECRET)
+        req.userId = payload.userId
+        next()
+    }catch(err){
+        return res.status(403).json({message: 'Token inválido ou expirado'})
+    }
+}
