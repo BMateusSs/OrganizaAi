cefee9df423c259ac06fc2bcd45480cd1432c4d4
BMateusSs <bmdss0810@gmail.com>
2025-10-02 19:16:54 -0300

    Adicionando rota para manipular tarefas

--- backend/src/app/server.js
+++ backend/src/app/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from '../rotas/public.js';
+import tasksRouter from '../rotas/tasks.js';
 import cors from 'cors'
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use(router)
+app.use('/tasks', tasksRouter)
 
 app.listen(3000, () => {
     console.log("Rodando o servidor na porta 3000")
