69cac464e3f47d4d9582d5de463f330c2deb9758
BMateusSs <bmdss0810@gmail.com>
2025-10-08 13:09:26 -0300

    Algumas atualizações na pagina today

--- frontend/main/js/today/script.js
+++ frontend/main/js/today/script.js
@@ -1,109 +1,133 @@
-import { createTask } from "../../../utils/createTasks.js";
+import { createTask, readTask } from "../../../utils/createTasks.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const content = document.getElementById('today');
+document.addEventListener('DOMContentLoaded', async () => {
+  const content = document.getElementById('today');
+  const todayList = document.createElement('div');
+  todayList.classList.add('today-list');
 
-    const todayList = document.createElement('div')
-    todayList.classList.add('today-list')
-    const button = document.createElement('button')
-    button.textContent = 'CRIAR'
-    content.appendChild(todayList)
-    content.appendChild(button)
+  const button = document.createElement('button');
+  button.textContent = '+ Adicionar tarefa';
+  button.classList.add('create-button', 'raleway-bold');
 
-    const taskModal = document.createElement('div') 
-    taskModal.classList.add("modal-overlay")
+  content.appendChild(todayList);
+  content.appendChild(button);
+
+  const taskModal = document.createElement('div');
+  taskModal.classList.add("modal-overlay");
 
 
-    const tasks = [
-        {title: 'Estudar para a prova'},
-        {title: 'Finalizar projeto Organiza-Aí'},
-        {title: 'Treinar dicção'}
-    ];
+  async function renderTasks() {
+    todayList.innerHTML = '';
+
+    const data = await readTask();
+    const tasks = await data.tasks;
 
     tasks.forEach(task => {
-        const card = document.createElement('div')
-        card.classList.add('task-card')
-        card.innerHTML=`
-            <input type="checkbox"/>
-            <h3 class="raleway-regular">${task.title}</h3>
-        `
-        todayList.appendChild(card)
+      const card = document.createElement('div');
+      card.classList.add('task-card');
+
+      const description = task.description || '';
+      const project = task.project_id || 'Aleatórias';
+
+      card.innerHTML = `
+        <div class="check-container">
+          <button class="check-button"></button>
+        </div>
+        <div class="content-task-container">
+          <h3 class="raleway-thin">${task.title}</h3>
+          <p class="raleway-thin">${description}</p>
+          <div class="info-task-container">
+            <div class="project-container raleway-thin">
+              <i class="fa-solid fa-box-archive"></i>
+              <span>${project}</span>
+            </div>
+          </div>
+        </div>
+      `;
+
+      const buttonCheck = card.querySelector('.check-button');
+      buttonCheck.addEventListener('click', () => {
+        buttonCheck.classList.toggle('checked');
+    
+      });
+
+      todayList.appendChild(card);
+    });
+  }
+
+  await renderTasks();
+
+  button.addEventListener('click', () => {
+    content.appendChild(taskModal);
+
+    const taskForm = document.createElement('div');
+    taskForm.classList.add('modal-content');
+    taskForm.innerHTML = `
+      <h2 class="modal-title raleway-bold">Criar Nova Tarefa</h2>
+      <div>
+        <label class="raleway-regular">Título da tarefa</label>
+        <input type="text" id="title" placeholder="Ex: Estudar para a prova"/>
+      </div>
+      <div>
+        <label class="raleway-regular">Descrição</label>
+        <textarea id="description" rows="4" class="raleway-thin" placeholder="Detalhes..."></textarea>
+      </div>
+      <div class="additionals-infos raleway-regular">
+        <select>
+          <option selected value="aleatoria">Aleatórias</option>
+          <option value="Organiza-Aí">Organiza-Aí</option>
+          <option value="javascript">Curso JavaScript</option>
+        </select>
+        <input type="date" id="due-date" class="raleway-thin"/>
+      </div>
+      <div class="buttons-container">
+        <button class="button cancel-button raleway-regular">Cancelar</button>
+        <button class="button confirm-button raleway-regular">Adicionar Tarefa</button>
+      </div>
+    `;
+    taskModal.appendChild(taskForm);
+
+    const confirmButton = taskForm.querySelector('.confirm-button');
+    confirmButton.addEventListener('click', async () => {
+      const user_id = 8;
+      const title = document.getElementById('title').value || null;
+      const description = document.getElementById('description').value || null;
+      const project_id = null;
+      const collumn_id = null;
+      const is_habit = false;
+      const recurrence = null;
+      const due_date = document.getElementById('due-date').value || null;
+
+      const task_info = {
+        user_id,
+        title,
+        description,
+        project_id,
+        collumn_id,
+        is_habit,
+        recurrence,
+        due_date
+      };
+
+      await createTask(task_info);
+
+      content.removeChild(taskModal);
+      taskModal.removeChild(taskForm);
+
+      await renderTasks();
     });
 
-    button.addEventListener('click', () => {
-        
+    const cancelButton = taskForm.querySelector('.cancel-button');
+    cancelButton.addEventListener('click', () => {
+      content.removeChild(taskModal);
+      taskModal.removeChild(taskForm);
+    });
 
-        content.appendChild(taskModal)
-
-        const taskForm = document.createElement('div')
-        taskForm.classList.add('modal-content')
-
-        taskForm.innerHTML=`
-        <h2 class="modal-title raleway-bold">Criar Nova Tarefa</h2>
-        <div>
-            <label class="raleway-regular">Título da tarefa</label>
-            <input type="text" id="title" placeholder="Ex: Estudar para a prova"/>
-        </div>
-        <div>
-            <label class="raleway-regular">Descrição</label>
-            <textarea id="description" name="descricao" rows="4" class="raleway-thin" placeholder="Detalhes, notas ou links úteis sobre a tarefa..."></textarea>
-        </div>
-        <div class="additionals-infos raleway-regular">
-            <select>
-                <option selected value="aleatoria" class="raleway-regular">Aleatórias</option>
-                <option value="Organiza-Aí" class="raleway-regular">Organiza-Aí</option>
-                <option value="javascript" class="raleway-regular">Curso JavaScript</option>
-            </select>
-            <input type="date" id="due-date" class="raleway-thin"/>
-        </div>
-        <div class="buttons-container">
-            <button class="button cancel-button raleway-regular">Cancelar</button>
-            <button class="button confirm-button raleway-regular">Adicionar Tarefa</button>
-        </div>
-        `
-
-        taskModal.appendChild(taskForm)
-
-        const confirmButton = document.querySelector('.confirm-button') 
-        confirmButton.addEventListener('click', () => {
-
-            const user_id = 8
-            const title = document.getElementById('title').value || null
-            const description = document.getElementById('description').value || null
-            const project_id = null
-            const collumn_id = null
-            const is_habit = false
-            const recurrence = null
-            const due_date = document.getElementById('due-date').value || null
-            
-            const task_info = {
-                user_id: user_id,
-                title: title,
-                description: description,
-                project_id: project_id,
-                collumn_id: collumn_id,
-                is_habit: is_habit,
-                recurrence: recurrence,
-                due_date: due_date
-            }
-
-            content.removeChild(taskModal)
-            taskModal.removeChild(taskForm)
-            createTask(task_info)
-            
-        })
-
-        const cancelButton = document.querySelector('.cancel-button') 
-        cancelButton.addEventListener('click', () => {
-            content.removeChild(taskModal)
-            taskModal.removeChild(taskForm)
-        })
-
-        taskModal.addEventListener('click', (event) => {
-            if (event.target === taskModal){
-                content.removeChild(taskModal)
-                taskModal.removeChild(taskForm)
-            }
-        })
-    })
-})
+    taskModal.addEventListener('click', (event) => {
+      if (event.target === taskModal) {
+        content.removeChild(taskModal);
+        taskModal.removeChild(taskForm);
+      }
+    });
+  });
+});
