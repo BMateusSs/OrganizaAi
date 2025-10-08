3801bd76b87ada06b0b4297dff67e059f79e5d7b
BMateusSs <bmdss0810@gmail.com>
2025-10-08 13:08:06 -0300

    Adicionando método get para as requisições

--- frontend/utils/methods.js
+++ frontend/utils/methods.js
@@ -1,11 +1,23 @@
 const token = localStorage.getItem('token')
-export function methodPost(body){
-    return{
+console.log(token)
+
+export function methodPost(body) {
+    return {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${token}` 
         },
         body: JSON.stringify(body)
+    }
+}
+
+export function methodGet() {
+    return {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
     }
 }
