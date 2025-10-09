9d8322f74a874816b1cc2f77a901287a3c064176
BMateusSs <bmdss0810@gmail.com>
2025-10-09 14:27:45 -0300

    commit

--- frontend/main/js/today/script.js
+++ frontend/main/js/today/script.js
@@ -1,4 +1,7 @@
-import { createTask, readTask } from "../../../utils/createTasks.js";
+import { createTask, readTask, updateTaskStatus } from "../../../utils/createTasks.js";
+import { getTodayDate } from "../../../utils/formatDates.js";
+import { taskFormModal } from "../../../utils/taskFormModal.js";
+
 
 document.addEventListener('DOMContentLoaded', async () => {
   const content = document.getElementById('today');
@@ -11,10 +14,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   content.appendChild(todayList);
   content.appendChild(button);
-
-  const taskModal = document.createElement('div');
-  taskModal.classList.add("modal-overlay");
-
 
   async function renderTasks() {
     todayList.innerHTML = '';
@@ -46,8 +45,12 @@ document.addEventListener('DOMContentLoaded', async () => {
       `;
 
       const buttonCheck = card.querySelector('.check-button');
-      buttonCheck.addEventListener('click', () => {
+      buttonCheck.addEventListener('click', async () => {
         buttonCheck.classList.toggle('checked');
+        const taskId = task.id
+        await updateTaskStatus(taskId)
+
+        await renderTasks()
     
       });
 
@@ -57,77 +60,10 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   await renderTasks();
 
-  button.addEventListener('click', () => {
-    content.appendChild(taskModal);
+  button.addEventListener('click', async () => {
+    const defaultDate = getTodayDate()
+    await taskFormModal(content, {defaultProject: 'javascript', defaultDate: defaultDate, onTaskCreated: renderTasks})
 
-    const taskForm = document.createElement('div');
-    taskForm.classList.add('modal-content');
-    taskForm.innerHTML = `
-      <h2 class="modal-title raleway-bold">Criar Nova Tarefa</h2>
-      <div>
-        <label class="raleway-regular">Título da tarefa</label>
-        <input type="text" id="title" placeholder="Ex: Estudar para a prova"/>
-      </div>
-      <div>
-        <label class="raleway-regular">Descrição</label>
-        <textarea id="description" rows="4" class="raleway-thin" placeholder="Detalhes..."></textarea>
-      </div>
-      <div class="additionals-infos raleway-regular">
-        <select>
-          <option selected value="aleatoria">Aleatórias</option>
-          <option value="Organiza-Aí">Organiza-Aí</option>
-          <option value="javascript">Curso JavaScript</option>
-        </select>
-        <input type="date" id="due-date" class="raleway-thin"/>
-      </div>
-      <div class="buttons-container">
-        <button class="button cancel-button raleway-regular">Cancelar</button>
-        <button class="button confirm-button raleway-regular">Adicionar Tarefa</button>
-      </div>
-    `;
-    taskModal.appendChild(taskForm);
-
-    const confirmButton = taskForm.querySelector('.confirm-button');
-    confirmButton.addEventListener('click', async () => {
-      const user_id = 8;
-      const title = document.getElementById('title').value || null;
-      const description = document.getElementById('description').value || null;
-      const project_id = null;
-      const collumn_id = null;
-      const is_habit = false;
-      const recurrence = null;
-      const due_date = document.getElementById('due-date').value || null;
-
-      const task_info = {
-        user_id,
-        title,
-        description,
-        project_id,
-        collumn_id,
-        is_habit,
-        recurrence,
-        due_date
-      };
-
-      await createTask(task_info);
-
-      content.removeChild(taskModal);
-      taskModal.removeChild(taskForm);
-
-      await renderTasks();
-    });
-
-    const cancelButton = taskForm.querySelector('.cancel-button');
-    cancelButton.addEventListener('click', () => {
-      content.removeChild(taskModal);
-      taskModal.removeChild(taskForm);
-    });
-
-    taskModal.addEventListener('click', (event) => {
-      if (event.target === taskModal) {
-        content.removeChild(taskModal);
-        taskModal.removeChild(taskForm);
-      }
-    });
   });
+  
 });
