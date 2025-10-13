f6ecc53161879f733b1e1436fbf0998e43befaa0
BMateusSs <bmdss0810@gmail.com>
2025-10-13 11:41:15 -0300

    Rotas relacionadas a projetos

---
+++ backend/src/rotas/projects.js
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { createProject, readProject } from '../database/querys/projects/projects.js'
+import { validateToken } from '../utils/auth.js'
+
+const projectRouter = Router()
+
+projectRouter.post('/create_project', validateToken, async (req, res) => {
+    try{
+        const userId = req.userId
+        const infos = req.body
+
+        const projectId = await createProject(userId, infos)
+
+        return res.status(201).json({'message': 'Projeto criado com sucesso', 'projectId': projectId})
+
+    }catch(error){
+        return res.status(500).json({'message': 'Erro ao criar projeto'})
+    }
+})
+
+projectRouter.get('/read_projects', validateToken, async (req, res) => {
+    try{
+        const userId = req.userId
+
+        const projects = await readProject(userId)
+
+        return res.status(200).json({projects})
+        
+    }catch(error){
+        return res.status(500).json({'message': 'Erro ao buscar projetos'})
+    }
+})
+
+export default projectRouter
