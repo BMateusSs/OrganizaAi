9570669c72021610df9d5638af22411923788492
BMateusSs <bmdss0810@gmail.com>
2025-10-13 11:35:31 -0300

    Nova função para formatar data

--- frontend/utils/formatDates.js
+++ frontend/utils/formatDates.js
@@ -5,3 +5,15 @@ export function getTodayDate() {
     const dd = String(today.getDate()).padStart(2, '0');
     return `${yyyy}-${mm}-${dd}`;
 }
+
+export function formatDate(dataISO) {
+  const data = new Date(dataISO);
+  
+  const meses = ["jan", "fev", "mar", "abr", "mai", "jun", 
+                 "jul", "ago", "set", "out", "nov", "dez"];
+  
+  const dia = data.getDate();
+  const mes = meses[data.getMonth()];
+
+  return `${dia} ${mes}`;
+}
