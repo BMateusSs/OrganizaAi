c5d989d127880a7c5b7afaf898ada4c4feb8de60
BMateusSs <bmdss0810@gmail.com>
2025-10-19 16:30:55 -0300

    Completando tela de tarefas concluídas

--- frontend/main/js/conclusions/conclusions.js
+++ frontend/main/js/conclusions/conclusions.js
@@ -1,4 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
+import { readCompletedTasks } from "../../../utils/createTasks.js"
+
+document.addEventListener('DOMContentLoaded', async () => {
     const conclusionsContainer = document.getElementById('check')
     conclusionsContainer.innerHTML="TAREFAS CONCLUÍDAS"
+
+    const completedTasks = await readCompletedTasks()
+
+    for (const [date, tasks] of Object.entries(completedTasks)){
+        const section = document.createElement('div')
+        const title = document.createElement('h1')
+        title.textContent=date
+        section.appendChild(title)
+
+        tasks.forEach(task => {
+
+            const description = task.description ? task.description : ''
+            const project = task.name ? task.name : 'Aleatórias'
+            
+            const cardTask = document.createElement('div')
+            cardTask.classList.add('task-card')
+            
+            cardTask.innerHTML=`
+            <div class="check-container">
+              <button class="check-button"></button>
+            </div>
+            <div class="content-task-container">
+              <h3 class="raleway-thin">${task.title}</h3>
+              <p class="raleway-thin">${description}</p>
+              <div class="info-task-container">
+                <div class="project-container raleway-thin">
+                  <i class="fa-solid fa-box-archive"></i>
+                  <span>${project}</span>
+                </div>
+              </div>
+            </div>
+          `;
+            
+            conclusionsContainer.appendChild(section)
+            
+            section.appendChild(cardTask)
+
+            const containerCheck = cardTask.querySelector('.check-container')
+            containerCheck.style.backgroundColor = task.code
+
+            const checkBtn = cardTask.querySelector('.check-button')
+            checkBtn.classList.add('checked')
+            checkBtn.style.backgroundColor = task.code
+        });
+    }
 })
