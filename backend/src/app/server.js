33f5849da2f33c26e560a67cb62025ebd6551660
BMateusSs <bmdss0810@gmail.com>
2025-10-20 21:55:54 -0300

    Rota para hábitos

--- backend/src/app/server.js
+++ backend/src/app/server.js
@@ -3,6 +3,7 @@ import router from '../rotas/public.js';
 import tasksRouter from '../rotas/tasks.js';
 import projectRouter from '../rotas/projects.js';
 import kanbansRouter from '../rotas/kanbans.js';
+import habitRouter from '../rotas/habits.js';
 import cors from 'cors'
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(router)
 app.use('/tasks', tasksRouter)
 app.use('/projects', projectRouter)
 app.use('/kanbans', kanbansRouter)
+app.use('/habits', habitRouter)
 
 app.listen(3000, () => {
     console.log("Rodando o servidor na porta 3000")
